3006e4345d7f8c81d0a9dede39ce4e1e273445d1
Gofannon <gofannon+github@riseup.net>
2022-08-18 21:47:53 +0200

    Change method to "automatic upgrade" of plugins

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,7 +17,7 @@ YNH_PHP_VERSION=8.1
 # For more information, see the PHP application helper: https://github.com/YunoHost/yunohost/blob/dev/helpers/php#L3-L6
 # Or this app package depending on PHP: https://github.com/YunoHost-Apps/grav_ynh/blob/master/scripts/_common.sh
 # PHP dependencies used by the app (must be on a single line)
-php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-gd"
+php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-cli"
 # or, if you do not need a custom YNH_PHP_VERSION:
 ###php_dependencies="php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
 
